722638dcd74a3f507b4ef403f228f0b7952e4315
Mohammed Ali <mohammed@aggregate-genius.com>
2023-08-20 19:45:58 -0700

    - added 'statistical_mode' unit tests

--- tests/testthat/test-Utils.R
+++ tests/testthat/test-Utils.R
@@ -1,3 +1,18 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("statistical_mode", {
+    # test no arguments
+    expect_error(statistical_mode())
+
+    # test invalid value
+    expect_equal(statistical_mode(""), "")
+    expect_equal(statistical_mode(NA), NA)
+    expect_null(statistical_mode(NULL))
+
+    # test single value
+    expect_equal(statistical_mode(1), 1)
+    expect_equal(statistical_mode("a"), "a")
+
+    # test vector
+    expect_equal(statistical_mode(mtcars$mpg), c(21, 22.8, 21.4, 19.2, 15.2, 10.4, 30.4))
+    expect_equal(statistical_mode(mtcars$cyl), 8)
+    expect_equal(statistical_mode(iris$Species), as.factor(c("setosa", "versicolor", "virginica")))
 })
